602af9e45d2d58f4a17774423a219b7e7f98068e
jw.yang <jw.yang@tianrang-inc.com>
2022-08-29 14:41:57 +0800

    初始化项目

---
+++ components/NavHeader/NavHeader.js
@@ -0,0 +1,18 @@
+// components/NavHeader.js
+Component({
+    properties: {
+        title: {
+            type: String,
+            value: "我是title默认值"
+        },
+        nav: {
+            type: String,
+            value: "我是nav默认值"
+        }
+    },
+    /**
+     * 页面的初始数据
+     */
+    data: {},
+    method: {}
+})
